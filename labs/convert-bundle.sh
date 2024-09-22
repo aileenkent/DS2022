b7f40a62822ec62ffcfe354b9e0d77e31e572c79
Aileen Kent <aileenmkent27@gmail.com>
2024-09-22 17:06:56 -0400

    convert tsv file bundle

---
+++ labs/convert-bundle.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wget https://s3.amazonaws.com/ds2002-resources/labs/lab3-bundle.tar.gz
+
+tar -xzvf lab3-bundle.tar.gz
+
+awk '!/^[[:space:]]*$/' lab3_data.tsv > cleaned-lab3-bundle.tsv
+
+tr '\t' ',' < cleaned-lab3-bundle.tsv > myfile.csv 
+
+data_lines=$(tail -n +2 myfile.csv | wc -l)
+
+echo "$data_lines"
+
+tar -czvf converted-archive.tar.gz myfile.csv
+
+
